9fcafaff4bb0cce5c1da9f42c4b9958971d87382
Akhil-Naraveni <naraveniakhil@gmail.com>
2025-12-31 14:00:06 +0530

    added basic wishlist functionality and updated styles for buttons and product cards

--- client/products_app/src/components/Products.jsx
+++ client/products_app/src/components/Products.jsx
@@ -1,19 +1,39 @@
 import React,{ useEffect, useState} from "react";
 import "./Products.css";
 import productIcon from "../../icons/product.svg"
+import wishlistSelectedIcon from "../../icons/wishlist_selected.svg"
+import wishlistUnselectedIcon from "../../icons/wishlist_not_selected.svg"
 const Products = () =>{
     const [productsList, setProductsList] = useState([]);
+    const [wishlist, setWishlist] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:5000/api/v1/products")
             .then((response) => response.json())
             .then((data) => setProductsList(data))
             .catch((error) => console.error("Error fetching products:", error));
+
     }, []);
 
-    const handleAddToCart = () => {
-        alert("Added to cart!");
+    const handleAddToCart = (product) => {
+        //alert(product.name);
+        fetch("http://localhost:5000/api/v1/cart/cartitems", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ productId: product._id, quantity: 1 }),
+        })
+
     };
+    const handleWishlist = (product) => {
+        if (wishlist.includes(product._id)) {
+            setWishlist(prevWishlist => prevWishlist.filter(id => id !== product._id));
+        } else {
+            setWishlist((prevWishlist) => [...prevWishlist, product._id]);
+        }
+    };
+
     return(
         <div className="main">
             <div className="head">
@@ -28,7 +48,11 @@ const Products = () =>{
                     <h4>{product.name}</h4>
                     <p>{product.description}</p>
                     <p className="price">Price: ${product.price}</p>
-                    <button onClick={handleAddToCart}>Add to Cart</button>
+                    <div className="btn-container">
+                        <button className="addtocartbtn" onClick={() => handleAddToCart(product)}>Add to Cart</button>
+                        <button onClick={() => handleWishlist(product)} className="wishlisticon"><img src={wishlist.includes(product._id) ? wishlistSelectedIcon : wishlistUnselectedIcon} alt="Wishlist Icon" width="24" height="30"/></button>
+                    </div>
+                    
                 </div>
             ))}
             </div>
