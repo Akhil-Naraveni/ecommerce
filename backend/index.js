b2e2aae8f7828d8836ec14923d48c5ad018cba5b
Akhil-Naraveni <naraveniakhil@gmail.com>
2026-01-27 20:53:03 +0530

    updated index.js Cors.

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,19 @@ require('dotenv').config();
 require('./database/database'); // Seed database on startup
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = { origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim()) : '*', 
-                    methods: ['GET','POST','PUT','DELETE','OPTIONS'], 
-                    credentials: true 
-                }; 
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'https://ecommerce-cart-yd8q.onrender.com'
+  ],
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  credentials: true
+};
+
 app.use(cors(corsOptions));
+
 // Connect to MongoDB
 connectDB();
 app.use(express.json());
