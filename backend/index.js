a029a7ea0c075885ada5797aa778b39154fb483e
Akhil-Naraveni <naraveniakhil@gmail.com>
2026-01-27 20:07:45 +0530

    Added a pre-flight options.

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 require('./database/database'); // Seed database on startup
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim()) : '*',
-};
-app.use(cors(corsOptions));
+const corsOptions = { origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim()) : '*', 
+                    methods: ['GET','POST','PUT','DELETE','OPTIONS'], 
+                    credentials: true 
+                }; 
+app.use(cors(corsOptions)); 
+app.options('*', cors(corsOptions));
 // Connect to MongoDB
 connectDB();
 app.use(express.json());
